92a2fe8bc509ff97d9b34663862bbd739828ffc5
Bri Ward <me@briward.com>
2025-12-05 18:03:50 +0000

    Export the new errors to mod.ts

--- mod.ts
+++ mod.ts
@@ -3,12 +3,27 @@
 import Kernel from "./src/kernel.ts";
 import Context from "./src/http/context.ts";
 import NotFound from "./src/error/not-found.ts";
+import Forbidden from "./src/error/forbidden.ts";
 import BadRequest from "./src/error/bad-request.ts";
 import ServerError from "./src/error/server-error.ts";
+import Unauthorized from "./src/error/unauthorized.ts";
 import ResponseManager from "./src/http/response-manager.ts";
+import MethodNotAllowed from "./src/error/method-not-allowed.ts";
+import UnprocessableEntity from "./src/error/unprocessable-entity.ts";
 
 // Export all available interfaces/types.
 export type { Error } from "./src/error/interfaces/error.ts";
 export type { Processor } from "./src/http/interfaces/processor.ts";
 
-export { BadRequest, Context, Kernel, NotFound, ResponseManager, ServerError };
+export {
+  Kernel,
+  Context,
+  NotFound,
+  Forbidden,
+  BadRequest,
+  ServerError,
+  Unauthorized,
+  ResponseManager,
+  MethodNotAllowed,
+  UnprocessableEntity,
+};
