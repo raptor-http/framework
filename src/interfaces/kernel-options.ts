a114066992e3443bbaa1d8bba62afcca75867c65
Bri Ward <me@briward.com>
2025-12-22 16:10:19 +0000

    Big tidy up and added lightweight error handling for responses.

---
+++ src/interfaces/kernel-options.ts
@@ -0,0 +1,9 @@
+/**
+ * Options which can be used to change kernel functionality.
+ */
+export interface KernelOptions {
+  /**
+   * Whether the system should automatically catch errors and respond.
+   */
+  catchErrors?: boolean;
+}
