18f95fa6a60c8497767e60c3b129952f3a5fc967
Bri Ward <me@briward.com>
2026-01-21 17:06:09 +0000

    Add new interface for response manager, allowing developers to override and add their own implementations.

---
+++ src/interfaces/response-manager.ts
@@ -0,0 +1,27 @@
+// interfaces/response-manager.ts
+import type Context from "../context.ts";
+import type { ResponseProcessor } from "./response-processor.ts";
+
+/**
+ * Manages the processing of response bodies into HTTP Response objects.
+ */
+export interface ResponseManager {
+  /**
+   * Process a body into a valid Response object.
+   *
+   * @param body A body value to process.
+   * @param context The current HTTP context.
+   *
+   * @returns A valid HTTP response object.
+   */
+  process(body: any, context: Context): Response | Promise<Response>;
+
+  /**
+   * Add a new processor to the response manager.
+   *
+   * @param processor An implementation of the response processor interface.
+   *
+   * @returns void
+   */
+  addProcessor(processor: ResponseProcessor): void;
+}
---
+++ src/interfaces/response-processor.ts
@@ -0,0 +1,27 @@
+// deno-lint-ignore-file no-explicit-any
+
+import type Context from "../context.ts";
+
+/**
+ * An HTTP response body processor.
+ */
+export interface ResponseProcessor {
+  /**
+   * The response body type the processor handles.
+   *
+   * @returns The body type.
+   */
+  type(): string;
+
+  /**
+   * Handle the response body and process.
+   *
+   * @param body Any HTTP response body.
+   * @param context The current HTTP context.
+   * @returns An HTTP response or null.
+   */
+  process(
+    body: any,
+    context: Context,
+  ): Promise<Response> | Response;
+}
